b58792cc343046ef0f08b9adf6c8a9cf5794e828
seeeB82 <sebastian.remer@googlemail.com>
2023-03-31 08:45:42 +0200

    Projektdateien hinzufügen.

---
+++ SimpleChat/Hubs/ChatHub.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Simple_Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+		// Speichert die aktiven Chat Clients
+		// static, da Hub jedes mal instanziiert wird
+		private static readonly Dictionary<string, string> chatClients = new Dictionary<string, string>();
+
+		public void SignOn(string username)
+		{
+
+			try
+			{
+				// Benutzer & Id merken
+				chatClients.Add(username, Context.ConnectionId);
+
+				// Alle Name an alle Clients versenden
+				Clients.All.SendAsync("handleSignOn", chatClients.Keys.ToArray());
+
+			}
+			catch (Exception e)
+			{
+				// TODO: Message anpassen
+				Clients.Caller.SendAsync("sendServerError", e.Message);
+			}
+		}
+
+		public void SendMessage(string message)
+		{
+			// Async entfernt
+			// ruft von allen verbundenen CLients die Methode "ReceiveMessage" auf
+			Clients.All.SendAsync("ReceiveMessage", message);
+		}
+
+
+		public override async Task OnDisconnectedAsync(Exception exception)
+		{
+			// Benutzer zu der ID ermitteln, welche die Verbidung geschlossen hat
+			KeyValuePair<string, string> user = chatClients.SingleOrDefault(kvp => kvp.Value == Context.ConnectionId);
+
+			// Benutzer aus dem Dicionary der Clients entfernen
+			chatClients.Remove(user.Key);
+
+			// Info an die verbleibenden Clients senden
+			await Clients.Others.SendAsync("handleSignOff", user.Key);
+
+			await base.OnDisconnectedAsync(exception);
+		}
+
+		//public void BroadcastMsg(string msg)
+		//{
+		//	// Aufruf der JavaScript-Funktion receiveMsg auf dem Client
+		//	Clients.All.SendAsync("receiveMsg", msg);
+		//}
+
+		//public void SendMsg(string user, string msg)
+		//{
+		//	// TODO Ausnahme prüfen falls Schlüssel nicht existiert
+		//	// ID des Benutzers ermitteln
+		//	string id = chatClients[user];
+
+		//	// Nachricht nur an den Client mit dieser Id versenden
+		//	Clients.Client(id).SendAsync("receiveMsg", msg);
+		//}
+
+
+		// Speichert die aktiven Chat Clients
+		// static, da Hub jedes mal instanziiert wird
+		//private static readonly Dictionary<string, string> chatClients = new Dictionary<string, string>();
+
+		//public void SignOn(string username)
+		//{
+
+		//    try
+		//    {
+		//        // Benutzer & Id merken
+		//        chatClients.Add(username, Context.ConnectionId);
+
+		//        // Alle Name an alle Clients versenden
+		//        //Clients.All.SendAsync("handleSignOn", chatClients.Keys.ToArray());
+
+		//    }
+		//    catch (Exception e)
+		//    {
+		//        // TODO: Message anpassen
+		//        Clients.Caller.SendAsync(e.ToString());
+		//    }
+		//}
+
+		//public override async Task OnDisconnectedAsync(Exception e)
+		//{
+		//    // ermittelt den Benutzer der ID, welche die Verbindung geschlossen hat
+		//    KeyValuePair<string, string> user = chatClients.SingleOrDefault(user => user.Value == Context.ConnectionId);
+
+		//    chatClients.Remove(user.Key);
+
+		//    // Information an aktive Benutzer senden
+		//    await Clients.Others.SendAsync("handleSignOff", user.Key);
+
+		//    await base.OnDisconnectedAsync(e);
+		//}
+
+
+	}
+}
---
+++ SimpleChat/wwwroot/js/site.js
@@ -0,0 +1,228 @@
+﻿
+// Deklaration globaler Variablen
+let chat;
+let username;
+
+// onClick-Event zum bestätigen des Benutzernamens
+$("#setUsername").click(setUsername);
+
+function setUsername() {
+
+    // speichern des Benutzernamens
+    username = $('#username').val();
+
+    // 
+    if (username != "") {
+        /*TODO: falls Username leer -> automatisch erzeugen*/
+    }
+
+    // Starten der Chat-FUnktion
+    chatStart();
+}
+
+function chatStart() {
+
+    // Start der Verbindung
+    chat = new signalR.HubConnectionBuilder()
+        .withUrl("/chatHub")
+        .build();
+
+
+    // .then wartet, bis Verbindung aufgebaut wurde
+    chat.start().then(() => {
+
+        // Benutzername an Server senden
+        chat.invoke("SignOn", username);
+
+
+        $('#sendButton').on("click", sendButtonClick);
+
+    });
+
+    chat.on("ReceiveMessage", function (message) {
+        let li = document.createElement("li");
+        li.textContent = message;
+
+        // Test
+        alert(message);
+
+        $("#messagesList").appendChild(li);
+
+        //document.getElementById("messagesList").appendChild(li);
+    });
+
+    
+
+}
+
+function sendButtonClick() {
+
+    let message = username + ": " + $('#messageInput').val();
+
+    alert(message);
+
+    chat.invoke("SendMessage", message);
+
+
+    //document.getElementById("sendButton").onclick = function () {
+        
+    //    let msgContent = document.getElementById("messageInput").value;
+    //    let message = username + ": " + msgContent;
+
+    //    chat.invoke("SendMessage", username, message);
+    //};
+}
+
+
+
+
+
+//let chat;
+
+//let username;
+
+//init();
+
+//function init() {
+//    // Benutzernamen einlesen
+//    $('#displayname').val(prompt('Bitte Namen eingeben:', ''));
+
+//    // ...und speichern
+//    username = $('#displayname').val();
+
+//    // Benutzername leer?
+//    if (username === undefined || username === '') {
+//        username = "ChatUser" + Math.floor(Math.random() * (100 - 1) + 1);
+//    }
+
+//    // Benutzername in die Seite einfügen
+//    $('#username').text(username);
+
+//    // Auswahl aufheben
+//    $('#deselect').on('click', () => $("#teilnehmer").val([]));
+
+//    chatStart();
+//}
+
+//function chatStart() {
+//    // Referenz auf den Hub
+//    chat = new signalR.HubConnectionBuilder()
+//        .withUrl("/chatHub")
+//        .build();
+
+//    // Methode für Fehlermeldung vom Server
+//    chat.on("sendServerError", (errorMessage) => {
+//        alert(errorMessage);
+//        init();
+//    });
+
+//    // Start der Verbindung
+//    // Das then() wartet bis die Verbindung aufgebaut wurde
+//    chat.start().then(() => {
+//        // Benutzernamen an den Server senden
+//        chat.invoke("SignOn", username);
+
+//        // Bei Buttonclick
+//        $('#sendmessage').on("click", sendButtonClick);
+//    });
+
+//    // Aufruf für den Erhalt einer Nachricht
+//    chat.on("receiveMsg", (message) => {
+//        // Nachricht einfügen
+//        $('#discussion').prepend(message + '\n');
+//    });
+
+//    // Aufruf für die Verbindung eines Benutzers
+//    chat.on("handleSignOn", (chatNames) => {
+//        // Teilnehmerliste löschen
+//        $("#teilnehmer").empty();
+//        $("#teilnehmer").append('<option disabled="disabled">Benutzer auswählen</option>');
+
+//        // Namen durchlaufen
+//        for (let i = 0; i < chatNames.length; i++) {
+//            // Alle Namen bis auf den eigenen...
+//            if (chatNames[i] !== username) {
+//                // ...in die Liste einfügen
+//                $("#teilnehmer").append('<option value="' + chatNames[i] + '">' + chatNames[i] + '</option>');
+//            }
+//        }
+//    });
+
+//    // Aufruf für den Verbindungsabbau eines Benutzers
+//    chat.on("handleSignOff", (chatName) => {
+//        // Teilnehmer aus dem select entfernen
+//        $("#teilnehmer option[value='" + chatName + "']").remove();
+//    });
+//}
+
+//function sendButtonClick() {
+//    // Nachricht auslesen
+//    let message = username + ": " + $('#message').val();
+
+//    // Ausgewählten Benutzer ermitteln
+//    let selectedUser = $('#teilnehmer option:selected').val();
+
+//    // An alle oder einen Benutzer?
+//    if (selectedUser === undefined) {
+//        // An alle Benutzer
+
+//        // Aufruf der Methode BroadcastMsg aus der ChatHub.cs
+//        chat.invoke("BroadcastMsg", message);
+
+//    }
+//    else {
+//        // An einen Benutzer
+
+//        // Aufruf der Methode SendMsg aus der ChatHub.cs
+//        chat.invoke("SendMsg", selectedUser, message);
+//    }
+
+//    // Textbox löschen und wieder den Fokus auf die Textbox setzten
+//    $('#message').val('').focus();
+//}
+
+
+//// Deklaration globaler Variablen
+//let chat;
+//let username;
+
+
+//function setUsername() {
+
+//    username = document.getElementById("username").value;
+
+//    if (username != "") {
+//        alert(username);
+//        console.log(username);
+//    }
+
+//    chatStart();
+//}
+
+//function chatStart() {
+
+//    let chat = new SignalR.HubConnectionBuilder()
+//        .withUrl("/chatHub")
+//        .build();
+
+//    chat.start().then(function () {
+
+//        // Benutzer an Server senden
+//        chat.invoke("SignOn", username);
+
+//        document.getElementById("sendButton").disabled = false;
+//    });
+
+//    chat.on("ReceiveMessage", function (message) {
+//        let msg = username + ": " + message;
+//        let li = document.createElement("li");
+//        li.textContent = msg;
+//        document.getElementById("messagesList").appendChild(li);
+//    });
+
+//    document.getElementById("sendButton").addEventListener("click", function (event) {
+//        let message = document.getElementById("messageInput").value;
+//        chat.invoke("SendMessage", message);
+//    });
+
+//}
